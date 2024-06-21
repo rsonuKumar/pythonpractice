7438e1f666aec4bdec4c3ce43b581fe3528aee07
ranson <rks12341995dnr@gmail.com>
2024-06-21 16:46:42 +0530

    chore:code update

---
+++ helloworld.py
@@ -0,0 +1,23 @@
+# Introduction
+print("hello , world")
+print("----------------------------------")
+#To get the version
+import sys
+print(sys.version)
+print("----------------------------------")
+# indentation 
+ # @ without error
+if 10>2:
+    print("Number 10 is greater then 2")
+print("----------------------------------")
+
+if(20<10):
+    print("20 is smaller")
+else:
+    print("10 is smaller")
+print("----------------------------------")
+
+# variable
+x,y=20,40
+print(x,y)
+print("----------------------------------")
